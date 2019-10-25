43324789cc70c6b0aa5baf12a820730926fb5a2f
Michael Ion <michael.ion.mi@outlook.com>
2019-10-25 15:48:53 -0400

    created assets folder with images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import About from './components/About'
 import Contact from './components/Contact'
 import Hours from './components/Hours'
@@ -7,8 +6,6 @@ import Location from './components/Location'
 import Menu from './components/Menu'
 import Ratings from './components/Ratings'
 import Map from './components/Map'
-
-
 import './App.css';
 import ReactMapGL from 'react-map-gl';
 
