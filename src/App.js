075fac0dc44fade57048c0e3b9396265464f2b4e
mattrlucciola <mr3lucciola@gmail.com>
2019-10-25 15:43:19 -0400

    all components + boilerplate code set in place

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+// react
 import React from 'react';
-import logo from './logo.svg';
+
+// styling
 import './App.css';
+
+// components
+import Menu from './components/menu/Menu';
+import Contact from './components/Contact';
+import Gallery from './components/Gallery';
+import Info from './components/Info';
+import Nav from './components/Nav';
+import PressReviews from './components/PressReviews';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      <Menu />
+      <Info />
+      <Gallery />
+      <PressReviews />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,18 @@
+// react
+import React from 'react'
+
+// components
+import Allergy from './Allergy';
+import Dessert from './Dessert';
+import Dinner from './Dinner';
+import Drinks from './Drinks';
+import Kitchen from './Kitchen';
+import Lunch from './Lunch';
+
+export default function Menu(){
+    return(
+        <div className="menu">
+
+        </div>
+    )
+}
