b8303326ab3da1c34a7bb5decb8f16d52aa74ee0
kitsufox9 <christinafox93@gmail.com>
2019-10-26 01:00:51 -0400

    minor changes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,21 @@
 // react
-import React from 'react'
+// import React from 'react'
 
 // components
-import Allergy from './Menus/Allergy';
-import Dessert from './Menus/Dessert';
-import Dinner from './Menus/Dinner';
-import Drinks from './Menus/Drinks';
-import Kitchen from './Menus/Kitchen';
-import Lunch from './Menus/Lunch';
+// import Allergy from './Menus/Allergy';
+// import Dessert from './Menus/Dessert';
+// import Dinner from './Menus/Dinner';
+// import Drinks from './Menus/Drinks';
+// import Kitchen from './Menus/Kitchen';
+// import Lunch from './Menus/Lunch';
 
-export default function Menu(){
-    return(
-        <div className="menu">
+// export default function Menu(){
+//     return(
+//         <div className="menu">
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
 
 // nathan
