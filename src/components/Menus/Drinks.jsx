d2ede37e620315b16852a4f95227c94abee78e63
npezzotti <npezzotti@hotmail.com>
2019-10-28 14:51:02 -0400

    some data rendering

--- src/components/Menus/Drinks.jsx
+++ src/components/Menus/Drinks.jsx
@@ -1,49 +1,55 @@
 import React from 'react'
 import json from './menu'
+
 const menu = json
+console.log(menu)
 
 export default function Drinks(){
     return(
-        <div className="menu-container">
-            <h3 className="menu-category">Soda/Juice</h3>
-            {menu.map(menuItem => {
+        <div className="menu-container" style={{color: "white"}}>
+        <h3 className="menu-category">Soda/Juice</h3>
+        {menu[4].Drinks[0].Soda_Juice.map(menuItem => {
                 return(
                     <div className="menu-info-container">
                         <div className="menu-item-container">
                             <div className="menu-item">{menuItem.item}</div>
+                            <div className="description">{menuItem.description}</div>
                             <div className="price">{menuItem.price}</div>
                         </div>
                     </div>
                 )
             })}
             <h3 className="menu-category">Beer</h3>
-            {menu.map(menuItem => {
+            {menu[4].Drinks[0].Beer.map(menuItem => {
                 return(
                     <div className="menu-info-container">
                         <div className="menu-item-container">
                             <div className="menu-item">{menuItem.item}</div>
+                            <div className="description">{menuItem.description}</div>
                             <div className="price">{menuItem.price}</div>
                         </div>
                     </div>
                 )
             })}
             <h3 className="menu-category">Sake</h3>
-            {menu.map(menuItem => {
+            {menu[4].Drinks[0].Sake.map(menuItem => {
                 return(
                     <div className="menu-info-container">
                         <div className="menu-item-container">
                             <div className="menu-item">{menuItem.item}</div>
+                            <div className="description">{menuItem.description}</div>
                             <div className="price">{menuItem.price}</div>
                         </div>
                     </div>
                 )
             })}
             <h3 className="menu-category">Wine</h3>
-            {menu.map(menuItem => {
+            {menu[4].Drinks[0].Wine.map(menuItem => {
                 return(
                     <div className="menu-info-container">
                         <div className="menu-item-container">
                             <div className="menu-item">{menuItem.item}</div>
+                            <div className="description">{menuItem.description}</div>
                             <div className="price">{menuItem.price}</div>
                         </div>
                     </div>
