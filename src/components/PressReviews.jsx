ff2c8fecf4e6dd8e2e1dcf0892d708987b9a14c6
kitsufox9 <christinafox93@gmail.com>
2019-10-28 16:49:04 -0400

    fixed press images

--- src/components/PressReviews.jsx
+++ src/components/PressReviews.jsx
@@ -5,21 +5,21 @@ export default function PressReviews(){
         <div className="press-reviews" id='press' style={{display: "grid"}}>
             <h1 style={{color: "white"}}>Press</h1>
             <div>
-                <img style={{width: "45%", height: "25%", marginRight: "500px"}} src={"/images/infatuation.png"} alt="infatuation" />
+                <img style={{width: "45%", height: "25%", marginRight: "500px"}} src={"/cropped_images/infatuation.png"} alt="infatuation" />
                 <p style={{marginLeft: "500px", color: "white"}}>
                     "Waiting in a line outside of any restaurant seems 
                     a bit silly to us, but Tomoe is arguably one of the best."
                 </p>
             </div>
             <div>
-                <img style={{width: "25%", height: "25%", marginRight: "500px"}} src={"/images/time-out-logo-png-1.png"} alt="time-out" />
+                <img style={{width: "25%", height: "25%", marginRight: "500px"}} src={"/cropped_images/time-out-logo-png-1.png"} alt="time-out" />
                 <p style={{marginLeft: "500px", color: "white"}}>
                     "The consistent freshness of the sushi and the range of the
                     a la carte menu has earned Tomoe an unimpeachable reputation."
                 </p>
             </div>
             <div>
-                <img style={{width: "25%", height: "25%", marginRight: "500px"}} src={"/images/Thrillist.png"} alt="thrillist" />
+                <img style={{width: "25%", height: "25%", marginRight: "500px"}} src={"/cropped_images/Thrillist.png"} alt="thrillist" />
                 <p style={{marginLeft: "500px", color: "white"}}>
                     "Despite its no-frills environment, diners queue up daily to 
                     get their hands on Tomoe's delicious rawness."
