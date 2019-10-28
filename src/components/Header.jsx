92bfcab552be8d7cf351fb04529521d256ef7d83
Michael Ion <michael.ion.mi@outlook.com>
2019-10-28 15:46:51 -0400

    fixed the header image much better now as bg instead of img and also started building from min-wid @media

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import hero from '../assets/images/Hero.jpg'
+// import hero from '../assets/images/Hero.jpg'
 
 export default function Header(){
     return(
         <div className="header" id='header'>
-            <img src={hero} alt="hero" className="mask"></img>
+            {/* <img src={hero} alt="hero" className="mask"></img> */}
         </div>
     )
 }
