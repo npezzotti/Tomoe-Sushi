15760490d0de7ac8108f566afddd92bc6b34d537
Michael Ion <michael.ion.mi@outlook.com>
2019-10-28 12:10:48 -0400

    changes to the nav layout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,15 @@ export default function Nav(){
     let time = `10:00PM`
     return(
         <div className="nav">
-<<<<<<< HEAD
             <div className='nav-time'>
-=======
-            <div className='nav-time'>Open Until {time}</div>
->>>>>>> ccb86220de51a68c3c2aabc664758f4b5b918f00
             <div className='nav-item-container'>
                 <a className='nav-item' href='#menu'>Menu</a>
                 <a className='nav-item' href='#info'>Info</a>
                 <a className='nav-item center' href='#home'>TOMOE SUSHI</a>
                 <a className='nav-item' href='#gallery'>Gallery</a>
                 <a className='nav-item' href='#press'>Press</a>
-<<<<<<< HEAD
                 <p>Open Until {time}</p>
             </div>
-=======
->>>>>>> ccb86220de51a68c3c2aabc664758f4b5b918f00
             </div>
         </div>
     )
