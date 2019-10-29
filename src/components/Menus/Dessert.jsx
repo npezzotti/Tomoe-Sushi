81473f062f3c83cc9a2efbe5fa4ab26fefb53453
npezzotti <npezzotti@hotmail.com>
2019-10-29 09:50:50 -0400

    merging

--- src/components/Menus/Dessert.jsx
+++ src/components/Menus/Dessert.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 import {  } from "module";
-import json from './menu'
-import { Icon } from '@opuscapita/react-icons';
+import json from './menu';
 
 const menu = json
 
 export default function Dessert(props){
     return(
         <div className="menu-container" style={{color: "white"}}>
-        <Icon className="remove-button" type="indicator" name="remove" onClick={() => props.changeMenu(null)} />/>
+        <div className="close-button" onClick={() => props.changeMenu(null)}>&times;</div>
             <h3 className="menu-category">Dessert</h3>
             {menu[0].Dessert[0].Dessert.map(menuItem => {
                 return(
