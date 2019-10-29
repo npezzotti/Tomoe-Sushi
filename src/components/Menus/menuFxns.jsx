d3b38ec6e2cfe3b109de818e757086e802d64775
mattrlucciola <mr3lucciola@gmail.com>
2019-10-29 12:26:11 -0400

    changing css quickly

--- src/components/Menus/menuFxns.jsx
+++ src/components/Menus/menuFxns.jsx
@@ -3,7 +3,7 @@ export const buildMenuInfoContainer = (menuItem) => {
     return(
         <div className="menu-info-container">
             <div className="menu-item-container">
-                <div className="menu-item">{menuItem.item}</div>
+                <div className="menu-item">{`${menuItem.item}`}&nbsp;-&nbsp;</div>
                 <div className="description">{menuItem.description}</div>
                 <div className="price">{menuItem.price}</div>
             </div>
