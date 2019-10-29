5bc752cec3c089c90d1793cecb57df88d4f7e2fa
mattrlucciola <mr3lucciola@gmail.com>
2019-10-29 16:53:22 -0400

    css for info, media queries

--- src/components/Menus/menuFxns.jsx
+++ src/components/Menus/menuFxns.jsx
@@ -4,7 +4,7 @@ export const buildMenuInfoContainer = (menuItem) => {
         <div className="menu-info-container">
             <div className="menu-item-container">
                 <div className="menu-item">{`${menuItem.item}`}&nbsp;-&nbsp;</div>
-                <div className="description">{menuItem.description}</div>
+                <div className="description">{`${menuItem.description}`.toLowerCase()}</div>
                 <div className="price">{menuItem.price}</div>
             </div>
         </div>
