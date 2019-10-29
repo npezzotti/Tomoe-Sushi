e3bcf8126db7ea0a9864326da80ed1e9437cd8a5
mattrlucciola <mr3lucciola@gmail.com>
2019-10-29 11:45:43 -0400

    included map in info.jsx, made css on info.jsx closer to spec, made info.jsx classes to match changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // react
-import React from 'react';
+import React, {useState} from 'react';
 
 // styling
 import './App.css';
 // import ReactMapGL from 'react-map-gl';
 
 // components
-import Map from './components/Map'
 import Header from './components/Header.jsx'
 import Menu from './components/Menu';
 import Contact from './components/Contact';
@@ -16,16 +15,16 @@ import Nav from './components/Nav';
 import PressReviews from './components/PressReviews';
 
 export default function App() {
+  let [timeDiff, setTimeDiff] = useState(``);
   return (
     <div className="App">
-      <Nav />
+      <Nav timeDiff={timeDiff} />
       <Header />
       <Menu />
-      <Info />
+      <Info dt={{setTimeDiff}} />
       <Gallery />
       <PressReviews />
       <Contact />
-      <Map />
     </div>
   );
 }
